331bc1c11efacaf2481c8489afbf76a1a3714034
ajrat <tlt_asm@mail.ru>
2020-03-18 17:53:13 +0400

    task2

--- src/main/java/Salakhov/Lesson/Task2/Main.java
+++ src/main/java/Salakhov/Lesson/Task2/Main.java
@@ -2,7 +2,17 @@ package Salakhov.Lesson.Task2;
 
 public class Main {
     public static void main(String[] args) {
+        int counter = 0;
 
        // Task 2 Написать код, который будет выбрасывать java.lang.StackOverflowError
+        recurced(counter);
     }
+
+    private static void recurced(int counter) {
+        counter++;
+        System.out.println(counter);
+        recurced(counter);
+
+    }
+
 }
