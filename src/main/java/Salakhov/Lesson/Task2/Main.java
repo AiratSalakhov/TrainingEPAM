06022f868bbfab415157fd4d823a2a82b45f53e8
ajrat <tlt_asm@mail.ru>
2020-03-18 15:53:50 +0400

    task1

---
+++ src/main/java/Salakhov/Lesson/Task2/Main.java
@@ -0,0 +1,8 @@
+package Salakhov.Lesson.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+
+       // Task 2 Написать код, который будет выбрасывать java.lang.StackOverflowError
+    }
+}
---
+++ src/main/java/Salakhov/Lesson/Task3/Main.java
@@ -0,0 +1,24 @@
+package Salakhov.Lesson.Task3;
+
+public class Main {
+       /*
+       Task 3
+       Написать свой загрузчик класса, который будет подгружать классы из директории вне вашего проекта.
+       - Создайте директорию myClasses вне вашего проекта
+               - В этой директории создайте класс TestClass, где переопределите метод toString, чтобы он выводил какой-то текст.
+               - Реализуйте загрузчик класса CustomClassLoader, который будет подгружать классы из директории myClasses.
+       При написании загрузчика нужно ориентироватся на последний слайд в презентации.
+       - Проверить, что ваш загрузчик классов работает корректно можно следующим способом:
+
+       CustomClassLoader classLoader = new CustomClassLoader();
+       Class clazz = classLoader.loadClass("TestClass");
+       Object obj = c.newInstance();
+       System.out.println(obj);
+
+       - Текст, который выведет метод println должен совпадать с текстом из переопределенного метода toString из класса TestClass
+        */
+       public static void main(String[] args) {
+
+       }
+
+}
