5dc97f7ced4ea5666bf9790c98d3762a206ebdc4
AiratSalakhov <tlt_asm1@mail.ru>
2020-04-21 07:28:02 +0400

    1

--- src/main/java/salakhov/lesson/Main.java
+++ src/main/java/salakhov/lesson/Main.java
@@ -1,6 +1,45 @@
 package salakhov.lesson;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+@Slf4j
 public class Main {
-//    public static void main(String[] args) {
-//     }
+    private static ResultSet resultSet;
+    private static Statement statement;
+    private static Connection connection;
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        try {
+
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost/ASM",
+                    "postgres", "GiveMe1000backs");
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from products;");
+            while (resultSet.next()) {
+                log.info(resultSet.getString(3));
+            }
+        } catch (SQLException e) {
+            log.info("SQL Exception {}", e.getMessage());
+        }
+        finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                log.info("SQL Exception {}", e.getMessage());
+            }
+        }
+
+    }
 }
